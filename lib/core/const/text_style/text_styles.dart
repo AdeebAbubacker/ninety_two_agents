b7ce43631161e961437dd03ab66a0954acc77574
Adeeb Abubacker <adeebabu655@gmail.com>
2024-08-12 15:12:08 +0530

    textstyle added

--- lib/core/const/text_style/text_styles.dart
+++ lib/core/const/text_style/text_styles.dart
@@ -9,19 +9,19 @@ class TextStyles {
   static TextStyle roboto16 = GoogleFonts.roboto(
     fontSize: 16,
   );
-   static TextStyle roboto16Color66 = GoogleFonts.roboto(
-    fontSize: 16,
-    color: const Color(0XFF666666)
-  );
+  static TextStyle roboto16Color66 =
+      GoogleFonts.roboto(fontSize: 16, color: const Color(0XFF666666));
   static TextStyle roboto12 = GoogleFonts.roboto(
     fontSize: 12,
   );
-   static TextStyle roboto12Color0D = GoogleFonts.roboto(
-    fontSize: 12,
-    color: const Color(0XFF488A0D)
-  );
+  static TextStyle roboto12Color0D =
+      GoogleFonts.roboto(fontSize: 12, color: const Color(0XFF488A0D));
   static TextStyle roboto14ColorFF = GoogleFonts.roboto(
     fontSize: 14,
     color: const Color(0XFFFFFFFF),
+  );
+  static TextStyle roboto14Color20 = GoogleFonts.roboto(
+    fontSize: 14,
+    color: const Color(0XFF42A120),
   );
 }
